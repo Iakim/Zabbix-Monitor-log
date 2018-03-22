d683ad8671fd2a4f39763a0ca079936728ec9281
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2018-03-22 12:11:38 -0300

    Create EventLog.ps1

---
+++ EventLog.ps1
@@ -0,0 +1,30 @@
+$id = 1503
+$name = "System"
+$new = "C:\new.txt"
+$old = "C:\old.txt"
+
+Get-EventLog -InstanceId $id -LogName $name -Newest 1 | Select-Object "Index" | Out-File C:\new.txt
+
+if (Test-Path $old){
+#echo "Arquivo old.txt existe"
+}
+else {
+New-Item $old -ItemType file
+echo "
+Iakim
+-----
+Isaac
+Moraes" > $old
+#echo "Arquivo old.txt criado"
+}
+
+if (Compare-Object -ReferenceObject $(Get-Content $new) -DifferenceObject $(Get-Content $old)) {
+Remove-Item -Path $old
+Rename-Item -NewName "old.txt" -Path $new
+echo "1"
+#echo "Alterado"
+}
+else{
+echo "0"
+#echo "Não alterado"
+}
